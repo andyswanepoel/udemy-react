c6962b2deef6692c569c3d34ed3febf62a1727ed
Andy Swanepoel <andyp.swanepoel@gmail.com>
2021-10-24 16:07:17 -0400

    Adding default className to Card component

--- UserApp/src/Components/UI/Card.js
+++ UserApp/src/Components/UI/Card.js
@@ -1,9 +1,9 @@
 import styles from "./Card.module.css";
 
-const Card = ({ elName, className, children }) => {
+const Card = ({ elName, className = "", children }) => {
   const Tag = `${elName}`;
 
-  return <Tag className={`${styles.card} ${className ?? ""}`}>{children}</Tag>;
+  return <Tag className={`${styles.card} ${className}`}>{children}</Tag>;
 };
 
 export default Card;
