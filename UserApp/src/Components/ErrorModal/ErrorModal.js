d528c93112a08e5b132723a3e89624dc32d55274
Andy Swanepoel <andyp.swanepoel@gmail.com>
2021-10-24 16:08:04 -0400

    Using ReactDOM createPortal on error modal

--- UserApp/src/Components/ErrorModal/ErrorModal.js
+++ UserApp/src/Components/ErrorModal/ErrorModal.js
@@ -1,26 +1,50 @@
+import { Fragment } from "react";
+import ReactDOM from "react-dom";
 import Button from "../UI/Button";
 import styles from "./ErrorModal.module.css";
-const ErrorModal = ({ errorMessage, onCloseModal }) => {
+
+const Backdrop = ({ onCloseModal }) => {
   return (
-    <div>
-      <div
-        className={styles["modal-background"]}
+    <div
+      className={styles["modal-background"]}
+      onClick={() => {
+        onCloseModal();
+      }}
+    ></div>
+  );
+};
+
+const ModalOverlay = ({ errorMessage, onCloseModal }) => {
+  return (
+    <div className={styles["modal-container"]}>
+      <h2 className={styles["modal-header"]}>Submission Error</h2>
+      <p className={styles["error-message"]}>{errorMessage}</p>
+      <Button
         onClick={() => {
           onCloseModal();
         }}
-      ></div>
-      <div className={styles["modal-container"]}>
-        <h2 className={styles["modal-header"]}>Submission Error</h2>
-        <p className={styles["error-message"]}>{errorMessage}</p>
-        <Button
-          onClick={() => {
-            onCloseModal();
-          }}
-        >
-          Close Modal
-        </Button>
-      </div>
+      >
+        Close Modal
+      </Button>
     </div>
+  );
+};
+
+const ErrorModal = ({ errorMessage, onCloseModal }) => {
+  return (
+    <Fragment>
+      {ReactDOM.createPortal(
+        <Backdrop onCloseModal={onCloseModal} />,
+        document.getElementById("backdrop-root")
+      )}
+      {ReactDOM.createPortal(
+        <ModalOverlay
+          errorMessage={errorMessage}
+          onCloseModal={onCloseModal}
+        />,
+        document.getElementById("overlay-root")
+      )}
+    </Fragment>
   );
 };
 
