090f939edfa7735b1d3d3c328d7559a352e5ac50
Andy Swanepoel <andyp.swanepoel@gmail.com>
2021-11-02 19:48:28 -0400

    FoodApp | Adding AvailableMeals component + child components

---
+++ FoodApp/src/components/Meals/MealItem/MealItem.js
@@ -0,0 +1,18 @@
+import MealItemForm from "./MealItemForm";
+import classes from "./MealItem.module.css";
+
+const MealItem = (props) => {
+  const price = `$${props.price.toFixed(2)}`;
+  return (
+    <li className={classes.meal}>
+      <div className={classes["meal-container"]}>
+        <h3>{props.name}</h3>
+        <p className={classes.description}>{props.description}</p>
+        <p className={classes.price}>{price}</p>
+      </div>
+      <MealItemForm id={props.id} />
+    </li>
+  );
+};
+
+export default MealItem;
