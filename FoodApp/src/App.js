78370c8ca47d441c57ec31c7b47619da353011be
Andy Swanepoel <andyp.swanepoel@gmail.com>
2021-11-02 22:14:33 -0400

    FoodApp | Adding Cart component with dummy data

--- FoodApp/src/App.js
+++ FoodApp/src/App.js
@@ -1,14 +1,25 @@
-import React from "react";
+import React, { useState } from "react";
 
 import Header from "./components/Layout/Header";
 import Meals from "./components/Meals/Meals";
+import Cart from "./components/Cart/Cart";
 
 function App() {
+  const [isCartOpen, setIsCartOpen] = useState(false);
+
+  const showCartHandler = () => {
+    setIsCartOpen(true);
+  };
+
+  const hideCartHandler = () => {
+    setIsCartOpen(false);
+  };
   return (
     <div>
-      <Header />
+      <Header showCart={showCartHandler} />
       <main>
         <Meals />
+        {isCartOpen === true && <Cart hideCart={hideCartHandler} />}
       </main>
     </div>
   );
