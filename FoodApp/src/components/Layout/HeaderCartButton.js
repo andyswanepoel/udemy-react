66750e64a221a2dfa7fa054f49805487f0fcf738
Andy Swanepoel <andyp.swanepoel@gmail.com>
2021-11-03 21:06:31 -0400

    FoodApp | Add small animation to cart

--- FoodApp/src/components/Layout/HeaderCartButton.js
+++ FoodApp/src/components/Layout/HeaderCartButton.js
@@ -1,18 +1,38 @@
-import React from "react";
-import { useContext } from "react";
+import React, { useContext, useEffect, useState } from "react";
+
 import CartContext from "../../store/cart-context";
 import CartIcon from "../Cart/CartIcon";
 import classes from "./HeaderCartButton.module.css";
 
 const HeaderCartButton = (props) => {
+  const [buttonAnimate, setButtonAnimate] = useState(false);
   const cartCtx = useContext(CartContext);
+  const { items } = cartCtx;
+  useEffect(() => {
+    if (items.length === 0) {
+      return;
+    }
+    setButtonAnimate(true);
 
-  const numberOfItems = cartCtx.items.reduce((acc, curr) => {
+    const timer = setTimeout(() => {
+      setButtonAnimate(false);
+    }, 300);
+
+    return () => {
+      clearTimeout(timer);
+    };
+  }, [items]);
+
+  const numberOfItems = items.reduce((acc, curr) => {
     return acc + curr.amount;
   }, 0);
 
+  const btnClasses = `${classes.button} ${
+    buttonAnimate === true ? classes.bump : ""
+  }`;
+
   return (
-    <button className={classes.button} onClick={() => props.showCart()}>
+    <button className={btnClasses} onClick={() => props.showCart()}>
       <span className={classes.icon}>
         <CartIcon />
       </span>
